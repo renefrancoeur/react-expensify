c06be811af39ed23d0457d40d0dccf875a5b4b66
rene francoeur <renefrancoeur@yahoo.com>
2018-08-28 16:39:39 -0400

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,52 @@
 const path = require('path');
 
-module.exports = {
-    //entry: './src/app.js',
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-     rules: [{
-         loader: 'babel-loader',
-         test: /\.js$/,
-         exclude: /node_modules/
-     },{
-         test: /\.s?css$/,
-         use: [
-             'style-loader',
-             'css-loader',
-             'sass-loader'
-         ]
-     }]   
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
-    },
-    mode: 'none'
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+         rules: [{
+             loader: 'babel-loader',
+             test: /\.js$/,
+             exclude: /node_modules/
+            },{
+             test: /\.s?css$/,
+             use: [
+                MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+         }]   
+        },
+        plugins: [
+            new MiniCssExtractPlugin({filename: "styles.css"})
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        },
+        mode: 'none'
+    };
 };
+
 
 // yarn run serve
 // yarn run build
