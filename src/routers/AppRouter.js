abadcf1aff4b1879e3815d5fedc9f402591527ab
rene francoeur <renefrancoeur@yahoo.com>
2018-09-11 17:03:49 -0400

    store user data in user database

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -7,6 +7,7 @@ import AddExpensePage from '../components/AddExpensePage';
 import LoginPage from '../components/LoginPage';
 import NotFoundPage from '../components/NotFoundPage';
 import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
 
 export const history = createHistory();
 
@@ -15,7 +16,7 @@ const AppRouter = () => {
     <Router history={history}>
         <div>
             <Switch>
-                <Route path="/" component={LoginPage} exact={true}/>
+                <PublicRoute path="/" component={LoginPage} exact={true}/>
                 <PrivateRoute path="/dashboard" component={ExpenseDashboardPage}/>
                 <PrivateRoute path="/create" component={AddExpensePage}/>
                 <PrivateRoute path="/edit/:id" component={EditExpensePage}/>
